68830355ccbce52224908ddf48a95d01b60dbf8c
Hayden Knapp <haydenknapp@gmail.com>
2018-05-10 20:20:47 -0400

    Final commit

---
+++ start.sh
@@ -0,0 +1,10 @@
+export FLASK_APP=microblog.py
+export FLASK_DEBUG=0
+export MAIL_SERVER=smtp.googlemail.com
+export MAIL_PORT=587
+export MAIL_USE_TLS=1
+export MAIL_USERNAME=
+export MAIL_PASSWORD=
+export MS_TRANSLATOR_KEY=
+flask run --port 80 --host '0.0.0.0'
+
